5c9e07621219dd03ad8a0b70014a7ec009e46ded
Flávio Castilho <flavio-castilho@outlook.com>
2021-03-10 10:35:25 -0300

    Acrescentada uma mensagem de bom dia

--- projeto-texte1/Program.cs
+++ projeto-texte1/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace projeto_texte1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
+
         }
     }
 }
