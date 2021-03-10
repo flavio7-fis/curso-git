f9afbd72910be1029e6fab492f61bd9646d44fb0
Flávio Castilho <flavio-castilho@outlook.com>
2021-03-10 11:26:35 -0300

    Acrescentada a mensagem desejando um bom dia

--- projeto-texte1/Program.cs
+++ projeto-texte1/Program.cs
@@ -9,9 +9,8 @@ namespace projeto_texte1 {
         static void Main(string[] args) {
 
             Console.WriteLine("Olá Mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
-
+            Console.WriteLine("Tenha um bom dia");
             Console.ReadLine();
 
         }
